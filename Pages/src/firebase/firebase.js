684043faca25624a056236451df71c187d9022c6
oxy-g3n <unstopabledestroyer77@gmail.com>
2024-08-11 21:07:06 +0530

    fixed a foolish mistake

--- Pages/src/firebase/firebase.js
+++ Pages/src/firebase/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBSAKeu4pyHhTmNon2aMhaBeGbxRvkKzcw",
-  authDomain: "astrology-de.firebaseapp.com",
-  projectId: "astrology-de",
-  storageBucket: "astrology-de.appspot.com",
-  messagingSenderId: "554952222010",
-  appId: "1:554952222010:web:81c8131d77b661733a5c03",
-  measurementId: "G-KRBY8E5TPX"
+  apiKey: "your stuff here",
+  authDomain: "your stuff here",
+  projectId: "your stuff here",
+  storageBucket: "your stuff here",
+  messagingSenderId: "your stuff here",
+  appId: "your stuff here",
+  measurementId: "your stuff here"
 };
 
 // Initialize Firebase
